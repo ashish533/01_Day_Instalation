c38335b8afcba7b4ba50d640f0045287a20cdb6f
ashish533 <108485504+ashish533@users.noreply.github.com>
2022-07-15 18:21:00 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,12 @@
+# print('hello world')
+# print('hello world')
+# print("hello world")
+# print('hello world')
+# print("hello world")
+# print('hello world')
+
+from cgi import print_directory
+
+
+a=5
+print(a)
